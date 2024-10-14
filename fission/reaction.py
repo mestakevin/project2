f9a92f5d11456b6b6c29d1ad7d46655fd9d2f267
Sara-Talebi <stalebi@g.syr.edu>
2024-10-14 16:58:56 -0400

    Update reaction.py
    updating the position based on the uranium element

--- fission/reaction.py
+++ fission/reaction.py
@@ -2,18 +2,18 @@
 import numpy as np
 
 # Method for fission reaction
-def fissionReaction(neutron_pos, Barium, Krypton, Neutron):
+def fissionReaction(uranium_pos, Barium, Krypton, Neutron):
     # When a neutron hits Uranium, simulate the fission
     print("Fission reaction occurred!")
     # Replace Uranium with fission products and release energy
     # Velocity increase are relative.
-    barium = Barium(neutron_pos, (10000 * np.random.rand(3)))  # Random initial velocity
-    krypton = Krypton(neutron_pos, (700 * np.random.rand(3)))
+    barium = Barium(uranium_pos, (10 * np.random.uniform(-1, 1, 3)))  # Random initial velocity
+    krypton = Krypton(uranium_pos, (7 * np.random.uniform(-1, 1, 3)))
     
     # Optionally, release more neutrons in random directions
-    new_neutron_1 = Neutron(neutron_pos, (14000 * np.random.rand(3)))
-    new_neutron_2 = Neutron(neutron_pos, (14000 * np.random.rand(3)))
-    new_neutron_3 = Neutron(neutron_pos, (14000 * np.random.rand(3)))
+    new_neutron_1 = Neutron(uranium_pos, (14 * np.random.uniform(-1, 1, 3)))
+    new_neutron_2 = Neutron(uranium_pos, (14 * np.random.uniform(-1, 1, 3)))
+    new_neutron_3 = Neutron(uranium_pos, (14 * np.random.uniform(-1, 1, 3)))
 
     # The fission products can be added to the particle list in the main simulation loop
     return [barium, krypton, new_neutron_1, new_neutron_2, new_neutron_3]
