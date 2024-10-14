b39d186554726bb8b083faccdc8c31185420c3e3
Sara-Talebi <stalebi@g.syr.edu>
2024-10-14 11:20:56 -0400

    Update particle.py
    adding the function scatterRandomly, which scatters particle after each collision (to other particle or wall)

--- fission/particle.py
+++ fission/particle.py
@@ -1,89 +1,96 @@
 #module for particles in simulation
 import math
 import numpy as np
+import random
 
 class Particle:
-        def __init__(self, pos, vel):
-            self.pos = pos
-            self.vel = vel 
-            self.mass = 1
-            self.radius = 0
-            self.eng = self.getEng()
+    def __init__(self, pos, vel):
+        self.pos = pos
+        self.vel = vel 
+        self.mass = 1
+        self.radius = 0
+        self.eng = self.getEng()
 
-        def move(self, drag_coeff, dt):
-            drag_force = 0.5 * drag_coeff * np.square(self.vel)
-          
-            accel = drag_force / self.mass
-           
-
-            new_vel = self.vel - accel * dt
-           
+    def move(self, drag_coeff, dt):
+        drag_force = 0.5 * drag_coeff * np.square(self.vel)
+        # Apply drag force to each component of velocity  
+        accel = drag_force / self.mass
+        
+        # Update velocity and position
+        new_vel = self.vel - (accel * dt)
+        self.updVel(new_vel)
+        self.updEng(self.getEng())
+        new_pos = self.pos + (new_vel * dt)
+        self.pos = new_pos
+        
+    def getEng(self):
+        return  0.5 * self.mass * self.getSpeed() ** 2
     
-            self.updVel(new_vel)
-            self.updEng(self.getEng())
+    def updEng(self, new_eng):
+        self.eng = new_eng
+    
+    def getVel(self):
+        return self.vel
 
-            new_pos = self.pos + new_vel * dt
-           
-            self.pos = new_pos
-           
+    def getSpeed(self):
+        return np.linalg.norm(self.getVel())
 
-        def getEng(self):
-            return  0.5 * self.mass * self.getSpeed() ** 2
+    def updVel(self, new_vel):
+        self.vel = new_vel
+
+    def getPos(self):
+        return self.pos
+    
+    def getRadius(self):
+        return self.radius
+
+    def distanceFrom(self,other_particle):
+        pos1 = self.getPos()
         
-        def updEng(self, new_eng):
-            self.eng = new_eng
+        x1,y1,z1= pos1[0], pos1[1], pos1[2]
+        pos2 = other_particle.getPos()
+        x2,y2,z2= pos2[0], pos2[1], pos2[2]
+        dist = math.sqrt( (x1-x2)**2 + (y1 -y2)**2 +(z1-z2)**2 )  
+        return dist
+
+    def collideParticle(self, other_particle):
+        min_dist = self.getRadius() + other_particle.getRadius() * 1.0e8
+        if self.distanceFrom(other_particle)  <= min_dist:
+            # Case 1: Both particles are of the same type (swap velocities and scatter)
+            if self.__class__.__name__ == other_particle.__class__.__name__:
+                vel_1 = self.getVel()
+                vel_2 = other_particle.getVel()
+
+                # Swap velocities
+                self.updVel(vel_2)
+                other_particle.updVel(vel_1)
+
+                # Scatter in random directions (apply random rotation to velocity vectors)
+                self.scatterRandomly()
+                other_particle.scatterRandomly()
+
+            # Case 2: Neutron and Uranium collision (fission event)
+            elif (self.__class__.__name__ == 'Neutron' and other_particle.__class__.__name__ == 'Uranium') or (self.__class__.__name__ == 'Uranium' and other_particle.__class__.__name__ == 'Neutron'):
+                self.fissionReaction()
+
+            # Case 3: Elastic collision with scattering for other types
+            else:
+                self.elasticCollision(other_particle)
+
+    # Method for random scattering
+    def scatterRandomly(self):
+        angle1 = random.uniform(0, 2 * np.pi)  # Random angle for scattering
+        angle2 = random.uniform(0, np.pi)
+        speed = self.getSpeed()
         
-        def getVel(self):
-            return self.vel
-
-        def getSpeed(self):
-            return np.linalg.norm(self.getVel())
-
-        def updVel(self, new_vel):
-            self.vel = new_vel
-
-        def getPos(self):
-            return self.pos
+        # Convert spherical to Cartesian coordinates
+        new_vel_x = speed * np.sin(angle2) * np.cos(angle1)
+        new_vel_y = speed * np.sin(angle2) * np.sin(angle1)
+        new_vel_z = speed * np.cos(angle2)
         
-        def getRadius(self):
-            return self.radius
-
-        def distanceFrom(self,other_particle):
-            pos1 = self.getPos()
+        new_vel = [new_vel_x, new_vel_y, new_vel_z]
+        self.updVel(new_vel)
             
-            x1,y1,z1= pos1[0], pos1[1], pos1[2]
-            pos2 = other_particle.getPos()
-            x2,y2,z2= pos2[0], pos2[1], pos2[2]
-            dist = math.sqrt( (x1-x2)**2 + (y1 -y2)**2 +(z1-z2)**2 )  
-            return dist
-
-
-        def collideParticle(self, other_particle):
-            min_dist = self.getRadius() + other_particle.getRadius() * 1.0e8
-            if self.distanceFrom(other_particle)  <= min_dist:
-                # Class names 
-                
-                # Case 1: Both particles are of the same type (swap velocities and scatter)
-                if self.__class__.__name__ == other_particle.__class__.__name__:
-                    vel_1 = self.getVel()
-                    vel_2 = other_particle.getVel()
-
-                    # Swap velocities
-                    self.updVel(vel_2)
-                    other_particle.updVel(vel_1)
-
-                    # Scatter in random directions (apply random rotation to velocity vectors)
-                    self.scatterRandomly()
-                    other_particle.scatterRandomly()
-
-                # Case 2: Neutron and Uranium collision (fission event)
-                elif (self.__class__.__name__ == 'Neutron' and other_particle.__class__.__name__ == 'Uranium') or (self.__class__.__name__ == 'Uranium' and other_particle.__class__.__name__ == 'Neutron'):
-                    self.fissionReaction()
-
-                # Case 3: Elastic collision with scattering for other types
-                else:
-                    self.elasticCollision(other_particle)
-        
         def collideWall(self,box_dim):
                 cur_vel = self.getVel()
                 if self.getRadius() * self.getPos()[0] > abs(box_dim[0]):
