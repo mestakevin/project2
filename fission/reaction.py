e33ad19d55fac0a8dbe39c5c497898f0bdc6dd59
Sara-Talebi <stalebi@g.syr.edu>
2024-10-14 13:15:50 -0400

    Update reaction.py
    debugging the errors due to circular imports between the particle.py and reaction.py

--- fission/reaction.py
+++ fission/reaction.py
@@ -1,20 +1,19 @@
 #fission reaction methods
 import numpy as np
-from particle import Barium, Krypton, Neutron, Uranium  
 
 # Method for fission reaction
-def fissionReaction(self):
+def fissionReaction(neutron_pos, Barium, Krypton, Neutron):
     # When a neutron hits Uranium, simulate the fission
     print("Fission reaction occurred!")
     # Replace Uranium with fission products and release energy
     # Velocity increase are relative.
-    barium = Barium(self.pos, (10000 * np.random.rand(3)))  # Random initial velocity
-    krypton = Krypton(self.pos, (700 * np.random.rand(3)))
+    barium = Barium(neutron_pos, (10000 * np.random.rand(3)))  # Random initial velocity
+    krypton = Krypton(neutron_pos, (700 * np.random.rand(3)))
     
     # Optionally, release more neutrons in random directions
-    new_neutron_1 = Neutron(self.pos, (14000 * np.random.rand(3)))
-    new_neutron_2 = Neutron(self.pos, (14000 * np.random.rand(3)))
-    new_neutron_3 = Neutron(self.pos, (14000 * np.random.rand(3)))
+    new_neutron_1 = Neutron(neutron_pos, (14000 * np.random.rand(3)))
+    new_neutron_2 = Neutron(neutron_pos, (14000 * np.random.rand(3)))
+    new_neutron_3 = Neutron(neutron_pos, (14000 * np.random.rand(3)))
 
     # The fission products can be added to the particle list in the main simulation loop
     return [barium, krypton, new_neutron_1, new_neutron_2, new_neutron_3]
