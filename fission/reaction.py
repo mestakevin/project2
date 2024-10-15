947f5a30c2db5a7e5943af6d1c2de3dd71fadb16
Sara-Talebi <stalebi@g.syr.edu>
2024-10-15 15:06:38 -0400

    Update reaction.py
    modification in fissionReaction function to fix the velocity issue

--- fission/reaction.py
+++ fission/reaction.py
@@ -7,13 +7,15 @@ def fissionReaction(uranium_pos, Barium, Krypton, Neutron):
     print("Fission reaction occurred!")
     # Replace Uranium with fission products and release energy
     # Velocity increase are relative.
-    barium = Barium(uranium_pos, (10 * np.random.uniform(-1, 1, 3)))  # Random initial velocity
-    krypton = Krypton(uranium_pos, (7 * np.random.uniform(-1, 1, 3)))
-    
+    pos = uranium_pos
+    velocity = [np.random.uniform(-1, 1, 3) for i in range(5)]
+    barium = Barium(pos, (10 * velocity[0]))  # Random initial velocity
+    krypton = Krypton(pos, (7 * velocity[1]))
+
     # Optionally, release more neutrons in random directions
-    new_neutron_1 = Neutron(uranium_pos, (14 * np.random.uniform(-1, 1, 3)))
-    new_neutron_2 = Neutron(uranium_pos, (14 * np.random.uniform(-1, 1, 3)))
-    new_neutron_3 = Neutron(uranium_pos, (14 * np.random.uniform(-1, 1, 3)))
+    new_neutron_1 = Neutron(pos, (14 * velocity[2]))
+    new_neutron_2 = Neutron(pos, (14 * velocity[3]))
+    new_neutron_3 = Neutron(pos, (14 * velocity[4]))
 
     # The fission products can be added to the particle list in the main simulation loop
     return [barium, krypton, new_neutron_1, new_neutron_2, new_neutron_3]
